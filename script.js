328fa61837e84a3bb104b1cad28d8efd3eb2fa46
iediong <ediongi6@hotmail.com>
2022-03-22 00:30:35 +0100

    Core Game logic done

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const checkBtn = document.querySelector('.check');
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = Number(document.querySelector('.score').textContent);
+
+checkBtn.addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  // console.log(guess, typeof guess);
+
+  if (!guess || guess > 20) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost the Game';
+      score = 0;
+      document.querySelector('.score').textContent = score;
+    }
+  } else {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You Lost the Game';
+      score = 0;
+      document.querySelector('.score').textContent = score;
+    }
+  }
+});
