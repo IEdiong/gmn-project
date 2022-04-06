91bb2db320fe06d40e41939aacb8278e58c846fe
iediong <ediongi6@hotmail.com>
2022-04-06 09:01:02 +0100

    Game set

--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,50 @@
-console.log('Typescript is working!!');
+const number = document.querySelector('.number');
+const messageEl = document.querySelector('.message');
+const score = document.querySelector('.score');
+const highscore = document.querySelector('.highscore');
+const checkBtn = document.querySelector('.check');
+const againBtn = document.querySelector('.again');
+
+// secret number
+let secretNumber = Math.floor(Math.random() * 20) + 1;
+let currentScore = 20;
+let playing = true;
+
+const resetGame = function () {
+  number.textContent = '?';
+  document.querySelector('.guess').value = '';
+  score.textContent = String(20);
+  currentScore = 20;
+  messageEl.textContent = 'Start guessing...';
+  secretNumber = Math.floor(Math.random() * 20) + 1;
+  playing = true;
+};
+
+// Check Button
+checkBtn!.addEventListener('click', function () {
+  if (playing) {
+    const guessNumber = +document.querySelector('.guess').value;
+
+    if (guessNumber === 0 || guessNumber > 20)
+      return (messageEl.textContent = 'Please make a guess!!');
+    else {
+      if (guessNumber === secretNumber) {
+        messageEl.textContent = 'Correct guess!!';
+        number!.textContent = String(secretNumber);
+        let currentHi = +highscore.textContent;
+        if (currentScore > currentHi) {
+          highscore.textContent = String(currentScore);
+        }
+        playing = false;
+      } else if (guessNumber !== secretNumber) {
+        messageEl.textContent =
+          guessNumber < secretNumber ? 'Too low' : 'Too high';
+        currentScore--;
+        score.textContent = String(currentScore);
+      }
+    }
+  }
+});
+
+// Again Button
+againBtn.addEventListener('click', resetGame);
--- js/app.js
+++ js/app.js
@@ -1,3 +1,47 @@
-"use strict";
-console.log('Typescript is working!!');
+var number = document.querySelector('.number');
+var messageEl = document.querySelector('.message');
+var score = document.querySelector('.score');
+var highscore = document.querySelector('.highscore');
+var checkBtn = document.querySelector('.check');
+var againBtn = document.querySelector('.again');
+// secret number
+var secretNumber = Math.floor(Math.random() * 20) + 1;
+var currentScore = 20;
+var playing = true;
+var resetGame = function () {
+    number.textContent = '?';
+    document.querySelector('.guess').value = '';
+    score.textContent = String(20);
+    currentScore = 20;
+    messageEl.textContent = 'Start guessing...';
+    secretNumber = Math.floor(Math.random() * 20) + 1;
+    playing = true;
+};
+// Check Button
+checkBtn.addEventListener('click', function () {
+    if (playing) {
+        var guessNumber = +document.querySelector('.guess').value;
+        if (guessNumber === 0 || guessNumber > 20)
+            return (messageEl.textContent = 'Please make a guess!!');
+        else {
+            if (guessNumber === secretNumber) {
+                messageEl.textContent = 'Correct guess!!';
+                number.textContent = String(secretNumber);
+                var currentHi = +highscore.textContent;
+                if (currentScore > currentHi) {
+                    highscore.textContent = String(currentScore);
+                }
+                playing = false;
+            }
+            else if (guessNumber !== secretNumber) {
+                messageEl.textContent =
+                    guessNumber < secretNumber ? 'Too low' : 'Too high';
+                currentScore--;
+                score.textContent = String(currentScore);
+            }
+        }
+    }
+});
+// Again Button
+againBtn.addEventListener('click', resetGame);
 //# sourceMappingURL=app.js.map
